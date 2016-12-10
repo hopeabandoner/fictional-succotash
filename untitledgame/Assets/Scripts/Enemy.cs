fe0718118dcbde35fcb5cddee02ed69809041ccf
WoolyDrool <nikolai.mcneely15@gmail.com>
2016-12-10 15:41:48 -0800

    commit4Dec10
    + added gunscript("HitScanWeapon.cs")
    ** needs to be fixed still

---
+++ untitledgame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public float health = 100;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //kill the enemy when their health is 0
+        if (health == 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+
+    //take damage
+    void ApplyDamage(float thedamage)
+    {
+        health -= thedamage;
+    }
+}
---
+++ untitledgame/Assets/Scripts/HitScanWeapon.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitScanWeapon : MonoBehaviour {
+    public float currentClip = 15;
+    public float allAmmo;
+    public float damage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        //Fire weapon
+        if(Input.GetButtonDown("Fire1"))
+        {
+            currentClip--;
+            RaycastHit hit;
+
+            //get the forward vector
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            //draw the raycast
+            if (Physics.Raycast(transform.position, fwd, out hit))
+            {
+                print("Hit something!");
+
+                //check if the raycast hit an enemy
+                if (hit.transform.tag == "Enemy")
+                {
+                    print("hit an enemy");
+                    //apply damage
+                    hit.transform.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+	}
+
+    void OnGUI()
+    {
+
+        //debug only: display ammo
+        GUI.Label(new Rect(10, 10, 128, 32), "Ammo: " + currentClip.ToString());
+
+    }
+}
